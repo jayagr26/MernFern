d53488a413075f2b6e6921421b30d855a6835dd6
Jay <jayagra@cisco.com>
2023-06-30 22:32:54 +0530

    refactored code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,127 +1,32 @@
-import ArticleIcon from "@mui/icons-material/Article";
-import CallSharpIcon from "@mui/icons-material/CallSharp";
-import HomeIcon from "@mui/icons-material/Home";
-import InfoIcon from "@mui/icons-material/Info";
-import MenuIcon from "@mui/icons-material/Menu";
-import {
-  AppBar,
-  Box,
-  Button,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  SwipeableDrawer,
-  Toolbar,
-  Typography,
-} from "@mui/material";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import React, { useState } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-import Display from "./components/Display";
-import Article from "./components/Article";
 import About from "./components/About";
+import Article from "./components/Article";
 import Contact from "./components/Contact";
-
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-
-const drawerList = [
-  { label: "Home", icon: <HomeIcon />, path: "/" },
-  { label: "Articles", icon: <ArticleIcon />, path: "/articles" },
-  { label: "About", icon: <InfoIcon />, path: "/about" },
-  { label: "Contact us", icon: <CallSharpIcon />, path: "/contact" },
-];
+import Display from "./components/Display";
+import AppBar from "./components/MyAppBar";
+import Drawer from "./components/MyDrawer";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   const [drawer, setDrawer] = useState(false);
-  const [focusedItem, setFocusedItem] = useState(0);
-
-  const handleKeyDown = (event) => {
-    if (event.key === "ArrowUp") {
-      event.preventDefault();
-      setFocusedItem((prev) => (prev > 0 ? prev - 1 : prev));
-    } else if (event.key === "ArrowDown") {
-      event.preventDefault();
-      setFocusedItem((prev) =>
-        prev < drawerList.length - 1 ? prev + 1 : prev
-      );
-    } else if (event.key === "Enter") {
-      event.preventDefault();
-      setDrawer(false);
-    } else if (event.key === "esc") {
-      setDrawer(false);
-    }
-  };
-
-  const list = () => (
-    <Box
-      sx={{ width: 250 }}
-      role="presentation"
-      onClick={() => {}}
-      onKeyDown={handleKeyDown}
-    >
-      <List>
-        {drawerList.map((item, index) => (
-          <ListItem
-            tabIndex={index}
-            onClick={() => {
-              setDrawer(false);
-            }}
-            sx={{
-              backgroundColor:
-                focusedItem === index ? "#e0e0e0" : "transparent",
-            }}
-            onMouseOver={() => setFocusedItem(index)}
-          >
-            <ListItemButton component={Link} to={item.path}>
-              <ListItemIcon>{item.icon}</ListItemIcon>
-              <ListItemText primary={item.label} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
 
   return (
     <Router>
-      <div className="App" tabIndex={-1} onKeyDown={handleKeyDown}>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-                onClick={() => setDrawer(true)}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                MernFern
-              </Typography>
-              <Button color="inherit">Login</Button>
-            </Toolbar>
-          </AppBar>
-        </Box>
+      <div className="App">
+        {/* AppBar */}
+        <AppBar setDrawer={setDrawer} />
 
-        <SwipeableDrawer
-          anchor={"left"}
-          open={drawer}
-          onClose={() => setDrawer(false)}
-          onOpen={() => setDrawer(true)}
-        >
-          {list()}
-        </SwipeableDrawer>
+        {/* SwipeableDrawer */}
+        <Drawer drawer={drawer} setDrawer={setDrawer} />
 
         <Routes>
           <Route path="/" element={<Display />} />
           <Route path="/articles" element={<Article />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/components/MyDrawer.js
@@ -0,0 +1,90 @@
+import ArticleIcon from "@mui/icons-material/Article";
+import CallSharpIcon from "@mui/icons-material/CallSharp";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  SwipeableDrawer,
+} from "@mui/material";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const drawerList = [
+  { label: "Home", icon: <HomeIcon />, path: "/" },
+  { label: "Articles", icon: <ArticleIcon />, path: "/articles" },
+  { label: "About", icon: <InfoIcon />, path: "/about" },
+  { label: "Contact us", icon: <CallSharpIcon />, path: "/contact" },
+];
+
+const MyDrawer = ({ drawer, setDrawer }) => {
+  const [focusedItem, setFocusedItem] = useState(0);
+  const navigate = useNavigate();
+
+  const handleKeyDown = (event) => {
+    if (event.key === "ArrowUp") {
+      event.preventDefault();
+      setFocusedItem((prev) => (prev > 0 ? prev - 1 : prev));
+    } else if (event.key === "ArrowDown") {
+      event.preventDefault();
+      setFocusedItem((prev) =>
+        prev < drawerList.length - 1 ? prev + 1 : prev
+      );
+    } else if (event.key === "Enter") {
+      event.preventDefault();
+      navigate(drawerList[focusedItem].path);
+      setDrawer(false);
+    } else if (event.key === "esc") {
+      setDrawer(false);
+    }
+  };
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={() => {}}
+      onKeyDown={handleKeyDown}
+    >
+      <List>
+        {drawerList.map((item, index) => (
+          <ListItem
+            tabIndex={index}
+            onClick={() => {
+              setDrawer(false);
+            }}
+            sx={{
+              backgroundColor:
+                focusedItem === index ? "#e0e0e0" : "transparent",
+            }}
+            onMouseOver={() => setFocusedItem(index)}
+          >
+            <ListItemButton component={Link} to={item.path}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.label} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div tabIndex={-1} onKeyDown={handleKeyDown}>
+      <SwipeableDrawer
+        anchor={"left"}
+        open={drawer}
+        onClose={() => setDrawer(false)}
+        onOpen={() => setDrawer(true)}
+      >
+        {list()}
+      </SwipeableDrawer>
+    </div>
+  );
+};
+
+export default MyDrawer;
